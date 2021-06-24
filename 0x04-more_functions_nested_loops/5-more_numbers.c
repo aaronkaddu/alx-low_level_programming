2b3be441e6c2586601143da9b51ea6de86ec0b8a
AaronKaddu <aaronmuzira@gmail.com>
2021-06-24 22:47:34 +0300

    fixed bug in 6

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,26 +1,19 @@
 #include "holberton.h"
-
 /**
- * more_numbers - prints 01234567891011121314\n x10
- *
- * Return: void
- */
-
+* more_numbers - prints 10 times the numbers, from 0 to 14.
+*/
 void more_numbers(void)
 {
-char a, b, c, i;
+	int i;
+	char c;
+
 	for (i = 0; i < 10; i++)
 	{
 		for (c = 0; c <= 14; c++)
 		{
-			if (c < 10)
-				b = c;
-			else
-			{
-				a = c / 10; b = c % 10;
-				_putchar('0' + a);
-			}
-			_putchar('0' + b);
+			if (c > 9)
+			_putchar((c / 10) + '0');
+			_putchar((c % 10) + '0');
 		}
 		_putchar('\n');
 	}
