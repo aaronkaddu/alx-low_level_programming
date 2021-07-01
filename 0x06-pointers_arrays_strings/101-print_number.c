854d5542a81e6d605619d7cb6fe43a9247e85770
AaronKaddu <aaronmuzira@gmail.com>
2021-07-01 21:49:15 +0300

    fixed another bug in print number

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,46 +1,35 @@
 #include "holberton.h"
 
 /**
- * print_number - takes an integer and prints it with _putchar
- * @n: integer to print
+ * print_number - printing number with puts
  *
- * Return: void
+ * @n: input int
  */
 void print_number(int n)
 {
-	int a0, a1, a2, a3, a4, a5, a6, a7, a8, a9;
-	int s0, s1, s2, s3, s4, s5, s6, s7, s8;
+	/*Declaring variables*/
+	int count = 0, pow = 1;
+	unsigned int num = n;
 
-	a0 = n / 1000000000; s0 = a0; a1 = (n / 100000000) % 10; s1 = s0 + a1;
-	a2 = (n / 10000000) % 10; s2 = s1 + a2;
-	a3 = (n / 1000000) % 10; s3 = s2 + a3;
-	a4 = (n / 100000) % 10; s4 = s3 + a4;
-	a5 = (n / 10000) % 10; s5 = s4 + a5;
-	a6 = (n / 1000) % 10; s6 = s5 + a6; a7 = (n / 100) % 10; s7 = s6 + a7;
-	a8 = (n / 10) % 10; s8 = s7 + a8; a9 = n % 10;
-	if (n < 0)
+
+	if (n < 0) /*Evaluate this condition*/
 	{
 		_putchar('-');
-		a0 *= -1; a1 *= -1; a2 *= -1; a3 *= -1; a4 *= -1;
-		a5 *= -1; a6 *= -1; a7 *= -1; a8 *= -1; a9 *= -1;
+		num = -n;
 	}
-	if (s0 != 0)
-		_putchar('0' + a0);
-	if (s1 != 0)
-		_putchar('0' + a1);
-	if (s2 != 0)
-		_putchar('0' + a2);
-	if (s3 != 0)
-		_putchar('0' + a3);
-	if (s4 != 0)
-		_putchar('0' + a4);
-	if (s5 != 0)
-		_putchar('0' + a5);
-	if (s6 != 0)
-		_putchar('0' + a6);
-	if (s7 != 0)
-		_putchar('0' + a7);
-	if (s8 != 0)
-		_putchar('0' + a8);
-	_putchar('0' + a9);
+	while (n != 0)
+	{
+		n /= 10;
+		count++;
+	}
+	while (count > 1)
+	{
+		pow *= 10;
+		count--;
+	}
+	while (pow >= 1)
+	{
+		_putchar(num / pow % 10 + '0');
+		pow /= 10;
+	}
 }
