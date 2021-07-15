5660adfb99b5830e656d4d722040eefcce81656a
AaronKaddu <aaronmuzira@gmail.com>
2021-07-15 23:40:35 +0300

    fixed some bugs in the file

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,38 +1,62 @@
 #include "holberton.h"
 #include <stdlib.h>
-#include <stdio.h>
 /**
- **string_nconcat - a function that concatenates two strings.
- *@s1 : pointer to string
- *@s2 : pointer to string
- *@n : integer
- *Return: pointer to string
+ * _strlen - give the length of a string
+ * @s: the string
+ *
+ * Return: the length of a string
+ */
+unsigned int _strlen(char *s)
+{
+	unsigned int i;
+
+	for (i = 0 ; s[i] != '\0' ; i++)
+		;
+	return (i);
+}
+
+/**
+ * string_nconcat - concatenates two strings
+ * @s1: string 1
+ * @s2: string 2
+ * @n: bytes of s2
+ *
+ * Return: a pointer with the content of s1 followed by n byte of s2
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-
-
-
-	unsigned int x1, x2, i, j;
-	char *s;
+	unsigned int length1, length2, i, j;
+	char *space;
 
 	if (s1 == NULL)
+	{
 		s1 = "";
+	}
 	if (s2 == NULL)
+	{
 		s2 = "";
-	for (x1 = 0; s1[x1] != '\0'; x1++)
-	;
-	for (x2 = 0; s2[x2] != '\0'; x2++)
-	;
-	if (n >= x2)
-		n = x2;
-	s = malloc(sizeof(char) * (x1 + n + 1));
-	if (s == NULL)
+	}
+
+	length1 = _strlen(s1);
+	length2 = _strlen(s2);
+
+	if (n >= length2)
+	{
+		n = length2;
+	}
+
+	space = malloc(sizeof(char) * (n + length1 + 1));
+
+	if (space == NULL)
+	{
 		return (NULL);
-	for (i = 0; s1[i] != '\0'; i++)
-		s[i] = s1[i];
-	for (j = x1; j < x1 + n; j++)
-		s[j] = s2[j - x1];
-	s[j] = '\0';
-	return (s);
+	}
+
+	for (i = 0 ; i < length1 ; i++)
+		space[i] = s1[i];
+	for (j = 0 ; s2[j] != '\0' && j != n ; j++, i++)
+		space[i] = s2[j];
+
+	space[i] = '\0';
+	return (space);
 }
